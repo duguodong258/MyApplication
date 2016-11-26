bfdf347249dd46a2f00b712877ff6c663e37734a
xianyu <836464017@qq.com>
2016-11-26 15:46:52 +0800

    第二次提交

--- app/src/main/java/com/example/administrator/myapplication/TestClass.java
+++ app/src/main/java/com/example/administrator/myapplication/TestClass.java
@@ -1,5 +1,7 @@
 package com.example.administrator.myapplication;
 
+import android.util.Log;
+
 /**
  * @author 咸鱼
  * @date 2016/11/26 0026.
@@ -7,5 +9,7 @@ package com.example.administrator.myapplication;
  */
 
 public class TestClass {
-
+    public void test(){
+        Log.i("TAG", "9999999999");
+    }
 }
